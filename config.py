99363c9f6a07e4d437ff66532e53aa8df1f0738a
Simone Podico <simpod@gmail.com>
2025-01-03 23:08:52 +0100

    added sample configuration file for application settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration file for the application
+# replace the values with the ones from your environment
+
+APP_SECRET_KEY = 'supersecretkey'
+
+PGHOST = 'postgres-host'
+PGUSER = 'postgres-user'
+PGDATABASE = 'postgres-db'
+PGPASSWORD = 'postgres-password'
